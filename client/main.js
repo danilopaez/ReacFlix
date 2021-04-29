9d89bec14b2d758830a18c72db987b0881e80205
Danilo Paez en Mac <danilo@epy.me>
2021-04-29 12:14:45 -0300

    Primer Aproximacion a RP...t1

--- client/main.js
+++ client/main.js
@@ -1,22 +1,25 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.Equipos.onCreated(function () {
+
+  Session.set('a', 1);
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Template.Equipos.helpers({
+  A() {
+
+    return Session.get('a');
   },
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+
+
+Template.Equipos.events({
+  'click button'() {
+
+    Session.set('a', Session.get('a') + 10)
   },
 });
